5042598f3ffcd583d9b6132296f09b17a545eaca
Tamanna <tamannarojha81@gmail.com>
2025-07-19 03:29:00 +0530

    fix: add mongoose import to user controller for database operations

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
